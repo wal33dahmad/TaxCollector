6a6e51cbdc15b06c19737dbdf7b51d1fb60feda9
wal33dahmad <rana.waleed123@gmail.com>
2023-04-04 14:36:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,439 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import "./App.css";
 
-function App() {
+const apiResponse = [
+  {
+    id: 14864,
+    tab_id: 3202,
+    name: "Recurring Item",
+    amount: 12.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.305Z",
+    updated_at: "2018-06-17T19:19:42.304Z",
+    type: "TabItem",
+    position: 1,
+    anchor: null,
+    parent_id: null,
+    catalog_object_id: null,
+    description: null,
+    available_quantity: null,
+    hidden: false,
+    options: {
+      recurring: {
+        enabled: true,
+        options: {
+          ends: { type: "never" },
+          start: { type: "first_payment" },
+          repeatInterval: "P1M",
+        },
+      },
+    },
+    tsv: "'item':2 'recur':1",
+    quantity_sold: 2,
+    amount_sold: 24.0,
+    total_buyers: 1,
+    quantity_refunded: 0,
+    amount_discounted: 0.0,
+    amount_refunded: 0.0,
+    net_amount: 24.0,
+    net_quantity: 2,
+    subcategory: null,
+    images: [],
+  },
+  {
+    id: 14865,
+    tab_id: 3202,
+    name: "Jasinthe Bracelet",
+    amount: 26.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.309Z",
+    updated_at: "2018-08-11T03:08:39.841Z",
+    type: "TabItem",
+    position: 1,
+    anchor: null,
+    parent_id: 14866,
+    catalog_object_id: 8463,
+    description: null,
+    available_quantity: 8,
+    hidden: false,
+    options: { makeAvailableQuantityPublic: false },
+    tsv: "'bracelet':2 'jasinth':1",
+    quantity_sold: 11,
+    amount_sold: 286.0,
+    total_buyers: 7,
+    quantity_refunded: 0,
+    amount_discounted: 0.0,
+    amount_refunded: 0.0,
+    net_amount: 286.0,
+    net_quantity: 11,
+    subcategory: null,
+    category: { id: 14866, name: "Bracelets", options: {} },
+    images: [
+      {
+        id: 5572,
+        upload_path: "uploads/image/image_file/716/jasinthe_bracelet.jpg",
+        metadata: {},
+        url: "https://images.cheddarcdn.com/eyJidWNrZXQiOiJjaGVkZGFyLXVwLXJldmlldyIsImtleSI6InVwbG9hZHMvaW1hZ2UvaW1hZ2VfZmlsZS83MTYvamFzaW50aGVfYnJhY2VsZXQuanBnIiwib3V0cHV0Rm9ybWF0IjoianBlZyIsImVkaXRzIjp7ImZsYXR0ZW4iOnsiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1fX19fQ==",
+      },
+    ],
+  },
+  {
+    id: 14867,
+    tab_id: 3202,
+    name: "Jasinthe Bracelet",
+    amount: 26.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.317Z",
+    updated_at: "2018-07-10T13:24:53.733Z",
+    type: "TabItem",
+    position: 2,
+    anchor: null,
+    parent_id: 14866,
+    catalog_object_id: 8463,
+    description: null,
+    available_quantity: 19,
+    hidden: false,
+    options: { makeAvailableQuantityPublic: true },
+    tsv: "'bracelet':2 'jasinth':1",
+    quantity_sold: 1,
+    amount_sold: 26.0,
+    total_buyers: 1,
+    quantity_refunded: 0,
+    amount_discounted: 5.2,
+    amount_refunded: 0.0,
+    net_amount: 20.8,
+    net_quantity: 1,
+    subcategory: null,
+    category: { id: 14866, name: "Bracelets", options: {} },
+    images: [
+      {
+        id: 5571,
+        upload_path: "uploads/image/image_file/1742/jasinthe_bracelet.jpg",
+        metadata: {},
+        url: "https://images.cheddarcdn.com/eyJidWNrZXQiOiJjaGVkZGFyLXVwLXJldmlldyIsImtleSI6InVwbG9hZHMvaW1hZ2UvaW1hZ2VfZmlsZS8xNzQyL2phc2ludGhlX2JyYWNlbGV0LmpwZyIsIm91dHB1dEZvcm1hdCI6ImpwZWciLCJlZGl0cyI6eyJmbGF0dGVuIjp7ImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NX19fX0=",
+      },
+    ],
+  },
+  {
+    id: 14868,
+    tab_id: 3202,
+    name: "Recurring Item with questions",
+    amount: 12.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.323Z",
+    updated_at: "2018-07-02T14:10:25.296Z",
+    type: "TabItem",
+    position: 2,
+    anchor: null,
+    parent_id: null,
+    catalog_object_id: null,
+    description: null,
+    available_quantity: null,
+    hidden: false,
+    options: {
+      recurring: {
+        enabled: true,
+        options: {
+          ends: { type: "payment_count", payment_count: 5 },
+          start: { type: "first_payment" },
+          repeatInterval: "P1M",
+        },
+      },
+      makeAvailableQuantityPublic: false,
+    },
+    tsv: "'item':2 'question':4 'recur':1",
+    quantity_sold: 0,
+    amount_sold: 0.0,
+    total_buyers: 0,
+    quantity_refunded: 0,
+    amount_discounted: 0.0,
+    amount_refunded: 0.0,
+    net_amount: 0.0,
+    net_quantity: 0,
+    subcategory: null,
+    images: [],
+  },
+  {
+    id: 14869,
+    tab_id: 3202,
+    name: "Zero amount item with questions",
+    amount: 0.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.334Z",
+    updated_at: "2018-07-02T14:09:09.267Z",
+    type: "TabItem",
+    position: 3,
+    anchor: null,
+    parent_id: null,
+    catalog_object_id: null,
+    description: null,
+    available_quantity: null,
+    hidden: false,
+    options: { makeAvailableQuantityPublic: false },
+    tsv: "'amount':2 'item':3 'question':5 'zero':1",
+    quantity_sold: 2,
+    amount_sold: 0.0,
+    total_buyers: 2,
+    quantity_refunded: 0,
+    amount_discounted: 0.0,
+    amount_refunded: 0.0,
+    net_amount: 0.0,
+    net_quantity: 2,
+    subcategory: null,
+    images: [],
+  },
+  {
+    id: 14870,
+    tab_id: 3202,
+    name: "Inspire Bracelet",
+    amount: 32.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.348Z",
+    updated_at: "2018-06-27T16:47:21.731Z",
+    type: "TabItem",
+    position: 3,
+    anchor: null,
+    parent_id: 14866,
+    catalog_object_id: 8462,
+    description: null,
+    available_quantity: 0,
+    hidden: false,
+    options: {},
+    tsv: "'bracelet':2 'inspir':1",
+    quantity_sold: 0,
+    amount_sold: 0.0,
+    total_buyers: 0,
+    quantity_refunded: 0,
+    amount_discounted: 0.0,
+    amount_refunded: 0.0,
+    net_amount: 0.0,
+    net_quantity: 0,
+    subcategory: null,
+    category: { id: 14866, name: "Bracelets", options: {} },
+    images: [
+      {
+        id: 5570,
+        upload_path: "uploads/image/image_file/715/inspire_bracelet.jpg",
+        metadata: {},
+        url: "https://images.cheddarcdn.com/eyJidWNrZXQiOiJjaGVkZGFyLXVwLXJldmlldyIsImtleSI6InVwbG9hZHMvaW1hZ2UvaW1hZ2VfZmlsZS83MTUvaW5zcGlyZV9icmFjZWxldC5qcGciLCJvdXRwdXRGb3JtYXQiOiJqcGVnIiwiZWRpdHMiOnsiZmxhdHRlbiI6eyJiYWNrZ3JvdW5kIjp7InIiOjI1NSwiZyI6MjU1LCJiIjoyNTV9fX19",
+      },
+    ],
+  },
+  {
+    id: 14872,
+    tab_id: 3202,
+    name: "Normal item with questions",
+    amount: 55.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.365Z",
+    updated_at: "2018-07-02T14:09:46.143Z",
+    type: "TabItem",
+    position: 4,
+    anchor: null,
+    parent_id: null,
+    catalog_object_id: null,
+    description: null,
+    available_quantity: null,
+    hidden: false,
+    options: { makeAvailableQuantityPublic: false },
+    tsv: "'item':2 'normal':1 'question':4",
+    quantity_sold: 2,
+    amount_sold: 110.0,
+    total_buyers: 2,
+    quantity_refunded: 0,
+    amount_discounted: 22.0,
+    amount_refunded: 0.0,
+    net_amount: 88.0,
+    net_quantity: 2,
+    subcategory: null,
+    images: [],
+  },
+  {
+    id: 14873,
+    tab_id: 3202,
+    name: "normal item ",
+    amount: 20.0,
+    amount_type: "fixed",
+    allow_quantity: null,
+    deleted_at: null,
+    required: false,
+    created_at: "2018-07-02T14:11:00.378Z",
+    updated_at: "2018-06-21T15:38:07.112Z",
+    type: "TabItem",
+    position: 5,
+    anchor: null,
+    parent_id: null,
+    catalog_object_id: null,
+    description: null,
+    available_quantity: null,
+    hidden: false,
+    options: {},
+    tsv: "'item':2 'normal':1",
+    quantity_sold: 1,
+    amount_sold: 20.0,
+    total_buyers: 1,
+    quantity_refunded: 0,
+    amount_discounted: 4.0,
+    amount_refunded: 0.0,
+    net_amount: 16.0,
+    net_quantity: 1,
+    subcategory: null,
+    images: [],
+  },
+];
+
+const formConfig = {
+  initialValues: {
+    applicable_items: [],
+    applied_to: "",
+    name: "",
+    rate: 0,
+  },
+  onSubmit: (values) => {
+    console.log(values);
+  },
+};
+
+const TaxResourceForm = () => {
+  const formik = useFormik(formConfig);
+
+  const handleSelectAll = (e) => {
+    const { checked } = e.target;
+    if (checked) {
+      formik.setFieldValue(
+        "applicable_items",
+        apiResponse.map((item) => item.id)
+      );
+      formik.setFieldValue("applied_to", "all");
+    } else {
+      formik.setFieldValue("applicable_items", []);
+      formik.setFieldValue("applied_to", "some");
+    }
+  };
+
+  const handleSelectSome = (e) => {
+    const { checked } = e.target;
+    if (checked) {
+      formik.setFieldValue("applicable_items", []);
+      formik.setFieldValue("applied_to", "some");
+    }
+  };
+
+  const handleSelectItem = (e) => {
+    const { value, checked } = e.target;
+    let applicable_items = [...formik.values.applicable_items];
+    if (checked) {
+      applicable_items.push(parseInt(value));
+    } else {
+      applicable_items = applicable_items.filter(
+        (id) => id !== parseInt(value)
+      );
+    }
+    formik.setFieldValue("applicable_items", applicable_items);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div style={{ width: "100%" }}>
+        <h1>Add Tax</h1>
+      </div>
+
+      <form onSubmit={formik.handleSubmit}>
+        <div className="input-container">
+          <input
+            className="name"
+            placeholder="Name"
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+          />
+          <div className="number-container">
+            <input
+              className="number"
+              placeholder="Rate"
+              type="number"
+              id="rate"
+              name="rate"
+              onChange={(e) =>
+                formik.setFieldValue("rate", (e.target.value / 100).toFixed(2))
+              }
+            />
+            <span className="percent">%</span>
+          </div>
+        </div>
+        <br />
+        <div>
+          <input
+            type="radio"
+            id="all"
+            name="applied_to"
+            value="all"
+            checked={formik.values.applied_to === "all"}
+            onChange={handleSelectAll}
+          />
+          <label htmlFor="all">Apply to all items in collection</label>
+          <br />
+          <input
+            type="radio"
+            id="some"
+            name="applied_to"
+            value="some"
+            checked={formik.values.applied_to === "some"}
+            onChange={handleSelectSome}
+          />
+          <label htmlFor="some">Apply to specific items</label>
+        </div>
+        <br />
+        <hr />
+        <div className="check-list">
+          {apiResponse.map((item) => (
+            <label key={item.id}>
+              <input
+                className="checkbox"
+                type="checkbox"
+                name="applicable_items"
+                value={item.id}
+                checked={
+                  formik.values.applied_to === "all" ||
+                  formik.values.applicable_items.includes(item.id)
+                }
+                onChange={handleSelectItem}
+              />
+              {item.name}
+            </label>
+          ))}
+        </div>
+        <br />
+        <hr />
+        <button type="submit">
+          Apply tax to {formik.values.applicable_items.length} item(s)
+        </button>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default TaxResourceForm;
